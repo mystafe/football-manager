ef55b3582231f8be56b6ddb3a8a720b20b2a2de3
mystafe <admess34@gmail.com>
2024-07-23 09:54:03 +0300

    v.3.3.2

--- src/utils/initializePlayerStats.jsx
+++ src/utils/initializePlayerStats.jsx
@@ -1,21 +1,15 @@
-import { calculatePlayerStrength } from './calculatePlayerStrength';
-
 export const initializePlayerStats = (teams) => {
-  const playerStats = teams.flatMap(team =>
-    team.players.map(player => {
-      const strength = calculatePlayerStrength(player);
-      if (isNaN(strength)) {
-        console.warn(`Calculated NaN strength for player: ${player.name}`);
-      }
-      return {
+  const playerStats = [];
+  teams.forEach(team => {
+    team.players.forEach(player => {
+      playerStats.push({
         name: player.name,
         team: team.name,
-        position: player.position,
-        strength: strength,
         goals: 0,
-        strengthChange: 0 // Güç değişikliklerini takip etmek için
-      };
-    })
-  );
+        strength: player.strength || 0,
+        position: player.position,
+      });
+    });
+  });
   return playerStats;
 };
--- src/utils/initializeTable.jsx
+++ src/utils/initializeTable.jsx
@@ -1,20 +1,15 @@
 import { calculateTeamStrength } from './calculateTeamStrength';
 
 export const initializeTable = (teams) => {
-  const table = teams.map(team => {
-    const strength = calculateTeamStrength({ name: team.name, players: team.players }) || 0;
-    return {
-      name: team.name,
-      strength: strength,
-      played: 0,
-      won: 0,
-      drawn: 0,
-      lost: 0,
-      goalsFor: 0,
-      goalsAgainst: 0,
-      points: 0
-    };
-  });
-  // console.log("Initialized Table:", table); // Konsola yazdır
-  return table;
+  return teams.map(team => ({
+    name: team.name,
+    strength: calculateTeamStrength(team),
+    played: 0,
+    won: 0,
+    drawn: 0,
+    lost: 0,
+    goalsFor: 0,
+    goalsAgainst: 0,
+    points: 0,
+  }));
 };
